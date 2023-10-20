b057bee51160af672ae8ffc00117a040f2f0bf18
AIE\S213846 <s213846@students.aie.edu.au>
2023-10-20 11:28:06 +1030

    Swapping working well.

--- Assets/Scripts/Adanna/Capturing/TrapPickUp.cs
+++ Assets/Scripts/Adanna/Capturing/TrapPickUp.cs
@@ -1,55 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class TrapDeploy : MonoBehaviour
+public class TrapPickUp : MonoBehaviour
 {
     public GameObject Trap;
     public GameObject PlayerGun;
     public GameObject Player;
 
-    [SerializeField]
-    bool holdingTrap;
-    bool holdingGun;
+    private Rigidbody _trapRig;
+
+    // Start is called before the first frame update
+
 
     enum CurrentlyHolding
     {
         vacuum,
         trap
     }
-
     [SerializeField] CurrentlyHolding currentlyHolding;
 
+    // Update is called once per frame
+   
     void Start()
     {
+        Trap.transform.position = PlayerGun.transform.position;
         currentlyHolding = CurrentlyHolding.vacuum;
 
+        _trapRig = Trap.GetComponent<Rigidbody>();
+        if (_trapRig != null)
+        {
+            Destroy(_trapRig);
+        }
+        Trap.transform.parent = Player.transform;
+        Trap.SetActive(false);
+
     }
-
-    // LB to toggle holding trap
-    // LT to throw trap if holding
-
-    // Update is called once per frame
     void Update()
     {
-        Toggle();
 
-        if(Input.GetMouseButtonDown(0))
-        {
-            DeployTrap();
-            
-        }
-        if (currentlyHolding == CurrentlyHolding.vacuum && Trap.transform.parent !=null)
-        {
-            Trap.transform.position = PlayerGun.transform.position;
-            Trap.transform.rotation = PlayerGun.transform.rotation;
-        }
-            
-        
+            DeployTrap();  
 
-
+      
     }
-
     void Toggle()
     {
         if (Input.GetKeyDown(KeyCode.R))
@@ -62,7 +56,7 @@ public class TrapDeploy : MonoBehaviour
                 return;
             }
 
-            if(currentlyHolding == CurrentlyHolding.trap)
+            if (currentlyHolding == CurrentlyHolding.trap)
             {
                 PlayerGun.SetActive(true);
                 Trap.SetActive(false);
@@ -70,37 +64,28 @@ public class TrapDeploy : MonoBehaviour
                 return;
             }
         }
-
-
     }
+
+
     void DeployTrap()
     {
-        if(currentlyHolding == CurrentlyHolding.trap)
+        
+        if (currentlyHolding == CurrentlyHolding.trap && Input.GetKeyUp(KeyCode.P))
         {
-            
+            Debug.Log("trap Deploying");
+            Trap.gameObject.AddComponent<Rigidbody>();
             Trap.transform.parent = null;
-            // Trap.transform.position = Player.transform.forward *5;
             currentlyHolding = CurrentlyHolding.vacuum;
-            Trap.AddComponent<Rigidbody>();
-            Debug.Log("deployyyyy");
-
         }
-    
+       
     }
+
 
     private void OnTriggerStay(Collider trap)
     {
         if (trap.gameObject.tag == "trap" && Input.GetKeyDown(KeyCode.Q))
         {
-
-            // holding trap == true 
-            // set vector position
-            // make trap a parent
-            Trap.AddComponent<Rigidbody>();
-            Trap.transform.parent = Player.transform;
-
+            Start();
         }
     }
-
-   
 }
