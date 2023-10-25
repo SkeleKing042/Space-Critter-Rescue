7671bc2c40aa789aaffee929385476161853eda8
Jackson Lucas <s210201@students.aie.edu.au>
2023-10-25 10:34:39 +1030

    Added Movement Demo scene.

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovment : MonoBehaviour
 {
     Rigidbody _rb;
