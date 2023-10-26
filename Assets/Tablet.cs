34777b73275c4212edcb1420d8a86284dba22b6c
AIE\s220749 <s220749@students.aie.edu.au>
2023-10-26 16:52:47 +1030

    Basic tablet

---
+++ Assets/Tablet.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Tablet : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    [SerializeField] TabletState tabletState;
+
+    [SerializeField] TabletTab tabletTab;
+    [SerializeField] GameObject[] tabs;
+    [SerializeField] int tabIndex;
+
+    public enum TabletState
+    {
+        on,
+        off,
+    }
+
+    enum TabletTab
+    {
+        Backpack,
+        Progress,
+        Options,
+        Controls,
+        Quit
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToggleTablet();
+        MoveTabs();
+    }
+
+    public void ToggleTablet()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(tabletState == TabletState.on)
+            {
+                TurnTabletOff();
+            }
+            else
+            {
+                TurnTabletOn();
+            }
+        }
+    }
+
+
+    public void SetTabletState(Tablet.TabletState inputTabletState)
+    {
+        tabletState = inputTabletState;
+    }
+
+    public void TurnTabletOff()
+    {
+        animator.SetTrigger("Lower Tablet");
+        SetTabletState(TabletState.off);
+    }
+
+    public void TurnTabletOn()
+    {
+        animator.SetTrigger("Raise Tablet");
+        SetTabletState(TabletState.on);
+    }
+
+
+
+    public void ActivateCorrectTab()
+    {
+        foreach(GameObject tab in tabs)
+        {
+            tab.SetActive(false);
+        }
+
+        tabs[tabIndex].SetActive(true);
+    }
+
+   /* public void MoveTabs()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && )
+        {
+
+        }
+    }*/
+
+}
