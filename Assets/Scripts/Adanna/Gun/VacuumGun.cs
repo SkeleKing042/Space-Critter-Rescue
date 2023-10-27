a908c71611250b195df94970bd6411a38b086bca
AIE\S213846 <s213846@students.aie.edu.au>
2023-10-27 11:51:39 +1030

    build made

--- Assets/Scripts/Adanna/Gun/VacuumGun.cs
+++ Assets/Scripts/Adanna/Gun/VacuumGun.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
-using static UnityEditor.PlayerSettings;
 
 public class VacuumGun : MonoBehaviour
 {
