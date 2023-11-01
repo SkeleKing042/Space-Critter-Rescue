87f300b74d0229c70c405daf9117c167768c3d9a
adanna <s213846@students.aie.edu.au>
2023-11-01 11:21:09 +1030

    Cleaning up code, twaeked the endpull function and the vacuum/trap enum decliration

---
+++ Assets/Scripts/collection/Gun/CollectAlien.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class CollectAlien : MonoBehaviour
+{
+    public TMP_Text AlienUICount;
+    private int AlienCount;
+    public bool mouseDown = false;
+
+    public VacuumGun Vac;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mouseDown = true;
+            //Vector3 position = PlayerGun.GetComponent<Vector3>;
+
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            mouseDown = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+       
+    }
+
+
+    void OnTriggerEnter(Collider creature)
+    {
+        Debug.Log("collision");
+        if ((creature.gameObject.tag == "alien" || creature.gameObject.tag == "bigAlien") && mouseDown)
+        {
+       
+          
+            AlienCount++;
+            AlienUICount.text = ": " + AlienCount.ToString();
+
+            Vac.UnassignAlien();
+
+            creature.transform.gameObject.SetActive(false);
+        }
+    }
+}
