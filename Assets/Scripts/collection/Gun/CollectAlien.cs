f16a54d4737cf4dd6c77a36fc9792beacd0d5601
adannaTouchesGrass <129356347+adannaTouchesGrass@users.noreply.github.com>
2023-11-09 16:01:14 +1030

    bug fixed

--- Assets/Scripts/collection/Gun/CollectAlien.cs
+++ Assets/Scripts/collection/Gun/CollectAlien.cs
@@ -15,17 +15,18 @@ public class CollectAlien : MonoBehaviour
     public bool mouseDown = false;
 
     [Header("Script Refrences")]
-    private VacuumGun Vac;
-    private Collection collection;
+    private VacuumGun _vac;
+    private Collection _collection;
+    private Trap _catchable;
 
 
     GameObject Alien;
 
     private void Start()
     {
-        Vac = FindObjectOfType<VacuumGun>();
-        collection = FindObjectOfType<Collection>();
-
+        _vac = FindObjectOfType<VacuumGun>();
+        _collection = FindObjectOfType<Collection>();
+        _catchable = FindObjectOfType<Trap>();
     }
 
 
@@ -52,15 +53,11 @@ public class CollectAlien : MonoBehaviour
     void OnTriggerEnter(Collider creature)
     {
 
-        if ((creature.gameObject.tag == "alien" || creature.gameObject.tag == "bigAlien") && Vac.Pulling == true)
+        if ((creature.gameObject.tag == "alien" || (creature.gameObject.tag == "bigAlien" && Trap.Catchable == true)) && _vac.Pulling == true)
         {
-            collection.AddAlienToCollection(creature.gameObject);
+            _collection.AddAlienToCollection(creature.gameObject);
 
-            // AlienCount++;
-            Alien = creature.gameObject;
-            
-
-            Vac.UnassignAlien(creature.gameObject);
+            _vac.UnassignAlien(creature.gameObject);
            // Destroy(creature.gameObject);
 
             //Vac.EndPull();
