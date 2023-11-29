5a199d5acdc14854c18737e076c260e6529406ac
adannaTouchesGrass <129356347+adannaTouchesGrass@users.noreply.github.com>
2023-11-29 14:08:51 +1030

    Made Testing Scene. Music refiing started

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -14,36 +14,56 @@ public class MusicManager : MonoBehaviour
         public Transform IslandPosition;
         public AudioSource IslandMusic;
 
+        [Tooltip("This is the distance where the player will start to hear the music on full volume")]
+        public int _maxMusicDistance;
+
+        [Tooltip("This is the distance where the player will start to hear the music on the lowest volume")]
+        public float _minMusicDistance;
+
+        [Tooltip("should be close the the min music distance for smooth transition form qiet to loud")]
+        public float _distanceVolumeScale;
+
     }
-
-    public GameObject Player;
+    public GameObject Player;   
     public List<IslandInfo> iData;
-
     // space music always playing
     // when in close radius only play specific music
+    private void Start()
+    {
 
+    }
     private void Update()
     {
         Volume();
     }
-
     void Volume()
     {
+        float _volumeScale;
         foreach (IslandInfo islands in iData)
         {
             islands.IslandDistance = Vector3.Distance(islands.IslandPosition.transform.position, Player.transform.position);
             islands.IslandDistance= Mathf.Abs(islands.IslandDistance);
 
-            if(islands.IslandDistance > 100)
+            if(islands.IslandDistance > islands._minMusicDistance)
             {
                 islands.IslandMusic.volume = 0;
             }
-            else
+
+            if(islands.IslandDistance > islands._maxMusicDistance && islands.IslandDistance <islands._minMusicDistance)
             {
-                islands.IslandMusic.volume = 100 / islands.IslandDistance;
+                _volumeScale = islands.IslandDistance /islands._distanceVolumeScale;
+
+                islands.IslandMusic.volume = 1 - _volumeScale;
+                Debug.Log(islands.IslandMusic.volume);
             }
 
-          
+
+            if(islands.IslandDistance <islands._maxMusicDistance)
+            {
+                islands.IslandMusic.volume = 1;
+                Debug.Log(islands.IslandMusic.volume);
+            }
+
 
         }
 
