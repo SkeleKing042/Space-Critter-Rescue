f9709eaee76c85fa8f8131d3ffeff279cb15078d
adannaTouchesGrass <129356347+adannaTouchesGrass@users.noreply.github.com>
2023-11-29 16:07:03 +1030

    The music Implentation for crystal and Fungi island is done. Other music to be added later. -Sopund Script basically not needed. Realised you can change the volume range in editor.

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -6,6 +6,8 @@ using static VacuumGun;
 
 public class MusicManager : MonoBehaviour
 {
+
+    // class that contains each varible for the music to scale properly
     [System.Serializable]
     public class IslandInfo
     {
@@ -24,7 +26,10 @@ public class MusicManager : MonoBehaviour
         public float _distanceVolumeScale;
 
     }
+
+    // player
     public GameObject Player;   
+    // list of islands
     public List<IslandInfo> iData;
     // space music always playing
     // when in close radius only play specific music
@@ -36,27 +41,34 @@ public class MusicManager : MonoBehaviour
     {
         Volume();
     }
+    /// <summary>
+    /// Will get the distance from each island and play the music louder depending on hous close the player gets
+    /// </summary>
     void Volume()
     {
+        // chnaging float value used to change the volume
         float _volumeScale;
         foreach (IslandInfo islands in iData)
         {
+
+            // get the distance from the player from the island
             islands.IslandDistance = Vector3.Distance(islands.IslandPosition.transform.position, Player.transform.position);
             islands.IslandDistance= Mathf.Abs(islands.IslandDistance);
 
+            // if too far away make the volume 0;
             if(islands.IslandDistance > islands._minMusicDistance)
             {
                 islands.IslandMusic.volume = 0;
             }
 
+            // if withing range scale volume depending on distance
             if(islands.IslandDistance > islands._maxMusicDistance && islands.IslandDistance <islands._minMusicDistance)
             {
+                // gets players distance and input distance to create a scale
                 _volumeScale = islands.IslandDistance /islands._distanceVolumeScale;
-
-                islands.IslandMusic.volume = 1 - _volumeScale;
+                islands.IslandMusic.volume = 1 -_volumeScale;
                 Debug.Log(islands.IslandMusic.volume);
             }
-
 
             if(islands.IslandDistance <islands._maxMusicDistance)
             {
