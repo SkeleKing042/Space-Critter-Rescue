6d7a2759f2d4cd0ef0125542993fd48c5f68ca59
adannaTouchesGrass <129356347+adannaTouchesGrass@users.noreply.github.com>
2023-12-06 11:53:14 +1030

    All sound functions added and tested. To be implemented once branch is ready

--- Assets/Scripts/Sound/SFXManager.cs
+++ Assets/Scripts/Sound/SFXManager.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SFXManager : MonoBehaviour
@@ -10,31 +11,41 @@ public class SFXManager : MonoBehaviour
     public bool Looping;
 
     public AudioSource Source;
+    public AudioSource TrapSource;
 
-    public List<AudioSource> CollectionSounds; // ill chage this to audio clip later.s
+    public List<AudioClip> CollectionSounds; // ill chage this to audio clip later.s
     public List<AudioClip> VacuumSounds;
     public List<AudioClip> JetpackSounds;
-               
+    public List<AudioClip> DetonatorSounds;
+    public List <AudioClip> TrapSounds;
+    public List<AudioClip> TabletSounds;
+
+
+    // varibkles need to mbe moved ONLY FOR TESTING
+
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Debug.Log(Looping);
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            CritterCollection();
+        }
+
+        //testing
     }
 
+    #region Collection
     public void CritterCollection()
     {
         // remeber to frefabe the sound manager
         int _soundplayed = UnityEngine.Random.Range(0, CollectionSounds.Count);
-        CollectionSounds[_soundplayed].Play();
+        Source.PlayOneShot(CollectionSounds[_soundplayed]);
     }
-
     public void SuckingSound(float TimePressed) 
     {
         Source.PlayOneShot(VacuumSounds[0],1f);
@@ -50,7 +61,9 @@ public class SFXManager : MonoBehaviour
         }
        
     }
+    #endregion
 
+    #region JetPack
     public void Jetpackflying(float TimePressed)
     {
         Source.PlayOneShot(JetpackSounds[0],1f);
@@ -70,4 +83,54 @@ public class SFXManager : MonoBehaviour
     {
         Source.PlayOneShot(JetpackSounds[3], 0.5f);
     }
+    #endregion
+
+
+    // Implement these functions when branch is free
+    #region Detonator
+    public void SwapDetonator()
+    {
+        Source.PlayOneShot(DetonatorSounds[0], 1f);
+    }
+    public void Detonator(bool TrapActive)
+    {
+        //playes detornator 
+        Source.PlayOneShot(DetonatorSounds[1], 1f);
+        if (!TrapActive)
+            TrapSource.PlayOneShot(TrapSounds[0], 1f); // TODO make this stop if the trap gets disabled.
+        else
+            TrapSource.Stop();
+    }
+    // may need to add a dectivate trap
+
+    #endregion
+
+    #region Trap
+
+    // put this inthe trap deplot function laterr
+    public void TrapDrop()
+    {
+        TrapSource.PlayOneShot(TrapSounds[1], 1f);
+    }
+    #endregion
+
+    #region Tablet
+    public void TabletOnOff(bool TabletActive)
+    {
+        if(!TabletActive)
+            Source.PlayOneShot(TabletSounds[0], 1f);
+        else
+            Source.PlayOneShot(TabletSounds[1], 1f);
+    }
+
+    public void TabletChange()
+    {
+        Source.PlayOneShot(TabletSounds[2], 1f);
+    }
+    public void Teleport()
+    {
+        Source.PlayOneShot(TabletSounds[3], 1f);
+    }
+
+    #endregion
 }
